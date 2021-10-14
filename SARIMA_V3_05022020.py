e07c607d7ae57b538c21c3221c30bd162d28b38e
Mainak Kundu <mainakmarket@gmail.com>
2021-10-14 10:05:37 +0530

    Update SARIMA_V3_05022020.py

--- SARIMA_V3_05022020.py
+++ SARIMA_V3_05022020.py
@@ -5,12 +5,6 @@ Created on Wed Feb  5 16:47:29 2020
 @author: mainak.kundu
 """
 
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Feb  4 12:13:04 2020
-
-@author: mainak.kundu
-"""
 
 import pandas as pd 
 import numpy as np 
@@ -175,50 +169,18 @@ def NonSeasonalSimulation(sales):
 
 
 if __name__== '__main__':
-    os.chdir('D:\PROJECTS\IM\SM_FORECASTING_12022020')
-    #hads = pd.read_csv('BA_HADS_31MAR2019.txt')
-    #lads = pd.read_csv('BA_LADS_31MAR2019.txt')
-    #text_file = open('/shared/sasusers/mainakk/file_ckeck.txt','w')
-    #text_file.writelines(p1)
-    #text_file.writelines(p2)
-    #text_file.writelines(p3)
-    #text_file.writelines(p4)
-    #text_file.writelines(p5)
-    #text_file.close()
+    os.chdir('path')
     import time
     import sys
     start = time.time()
-    #input_path = p1
-    #output_path = p2 
-    #print(input_path)
-    #path = str(input_path)
-    #print(input_path)
-    #path = str(input_path)
-    #USR = p3 ## args
-    #CNCPT = p4 ## args
-    #DEPT = p5 ## args
-    #logs = open(USR+'_'+CNCPT+'_'+DEPT+'_'+'SARIMA_LOGS.txt','w')
-    #sys.stdout = logs
-    print('---Path Declaration---')
-    #INPUT_PATH = p1+'/'+USR+'_'+CNCPT+'_'+DEPT
-    #OUTPUT_PATH = p2+'/'+USR+'_'+CNCPT+'_'+DEPT
-    #hads_path = 
-    #lads_path = OUTPUT_PATH+'_FCST_LADS.txt'
-    #print(hads_path)
-    #print(lads_path)
-    #exit()
-    h = pd.read_csv('PYL_SM_LDS_FCST_HADS.txt')
-    l = pd.read_csv('PYL_SM_LDS_FCST_LADS.txt')
-    
-    
-    #h = hads
-    #l = lads
+    h = pd.read_csv('train.txt')
+    l = pd.read_csv('test.txt')
 
     print('=== Stationarity Tagging ===')
     resultF    = pd.DataFrame()
-    terr = [v for v in h['STND_TRRTRY_NM'].unique()]
+    terr = [v for v in h['trrtry'].unique()]
     for ter in terr:
-        terrData   = h[h.STND_TRRTRY_NM==ter]
+        terrData   = h[h.trrtry==ter]
         optionList = list(set(terrData.KEY))
         #optionList = optionList[0:4]
         print(len(optionList))
@@ -226,15 +188,15 @@ if __name__== '__main__':
             
             result   = pd.DataFrame()
             print('In progress' +'\n'+ter + '-----'+option )
-            histData = h[(h.STND_TRRTRY_NM==ter)& (h.KEY==option)]
+            histData = h[(h.trrtry==ter)& (h.KEY==option)]
             if len(histData) > 52:
-                t = St_tagger(histData,'RTL_QTY')
+                t = St_tagger(histData,'QTY')
                 result['ST_TGGR'] = t
-                result['STND_TRRTRY_NM'] = ter
+                result['trrtry'] = ter
                 result['KEY'] = option
             else:
                 result['ST_TGGR'] ='NOTH'
-                result['STND_TRRTRY_NM'] = ter
+                result['trrtry'] = ter
                 result['KEY'] = option
             resultF = pd.concat([result,resultF])
             print('== Done Stationarity Tagging ==')   
@@ -245,16 +207,16 @@ if __name__== '__main__':
 
     print('==== Option Level Seasonal Tagging  ====')
     seasF    = pd.DataFrame()
-    terr = [v for v in h1['STND_TRRTRY_NM'].unique()]
+    terr = [v for v in h1['trrtry'].unique()]
     for ter in terr:
-            terrData   = h1[h1.STND_TRRTRY_NM==ter]
+            terrData   = h1[h1.trrtry==ter]
             optionList = list(set(terrData.KEY))
             #optionList = optionList[0:5]
             print(len(optionList))
             for ind,option in enumerate(optionList):
                 result   = pd.DataFrame()
                 print('In progress' +'\n'+ter + '-----'+option )
-                histData = h1[(h1.STND_TRRTRY_NM==ter)& (h1.KEY==option)]
+                histData = h1[(h1.trrtry==ter)& (h1.KEY==option)]
                 if len(histData) > 104:
                     per = SeasonalityChk_Modf(histData)
                     histData['SEASONAL_OVERLAP'] = per                         
@@ -262,28 +224,28 @@ if __name__== '__main__':
                     seasF = pd.concat([result,seasF])
     print('===SEASONAL_COMPONENTS TAGGING COMPLETE ===')
 
-    k = seasF.groupby(['STND_TRRTRY_NM','SUB_CLSS_NM'])['SEASONAL_OVERLAP'].agg(pd.Series.max).reset_index()
+    k = seasF.groupby(['trrtry','SUB_CLSS'])['SEASONAL_OVERLAP'].agg(pd.Series.max).reset_index()
     k['IS_SNL'] = np.where(k['SEASONAL_OVERLAP']>0,'NO','YES') ### this will help to run auto.arima 
-    h1 = h1.merge(k,how='left',on=['STND_TRRTRY_NM','SUB_CLSS_NM'])
+    h1 = h1.merge(k,how='left',on=['trrtry','SUB_CLSS_NM'])
     print(h1.shape)
-    h1.to_csv('USR_CNCPT_DEPT_HADS.txt') ### this will consume by auto.arima code 
+    h1.to_csv('seasonal_params.txt') ### this will consume by auto.arima code 
     
-    print('== Seasonal Data Bifarcated ==')
+    print('== Seasonal Data selected ==')
     h_snl = h1[h1['IS_SNL']=='YES'] ### roll up that data and do sub-class level simulation 
     print(h_snl.shape)
 
     print('== Seasonal Simulation starts on Sub-class-Level ==')
     order_temp    = []
-    new_h = h_snl.groupby(['STND_TRRTRY_NM','SUB_CLSS_NM','TRDNG_WK_END_DT'])['RTL_QTY'].mean().reset_index() ## sub class level rollup
-    terr = [v for v in new_h['STND_TRRTRY_NM'].unique()]
+    new_h = h_snl.groupby(['trrtry','SUB_CLSS','date'])['QTY'].mean().reset_index() ## sub class level rollup
+    terr = [v for v in new_h['trrtry'].unique()]
     for ter in terr:
-        terrData   = new_h[new_h.STND_TRRTRY_NM==ter]
-        clss_lst = list(set(terrData.SUB_CLSS_NM))
+        terrData   = new_h[new_h.trrtry==ter]
+        clss_lst = list(set(terrData.SUB_CLSS))
         for i in clss_lst:
             print('In progress' +'\n'+ter + '-----'+str(i) )
-            histData = new_h[(new_h.STND_TRRTRY_NM==ter)& (new_h.SUB_CLSS_NM==i)]
+            histData = new_h[(new_h.trrtry==ter)& (new_h.SUB_CLSS==i)]
             if len(histData) >= 104:
-                sales = histData['RTL_QTY']
+                sales = histData['QTY']
                 sales = np.where(sales==0,1,sales)
                 sales = np.log(sales)
                 seasonal_order,order= SeasonalSimulation(sales)
@@ -298,33 +260,33 @@ if __name__== '__main__':
     print(orderdf.head(2))
     orderdf['P'],orderdf['D'],orderdf['Q'],orderdf['m']=zip(*orderdf['S_ORDER'].map(split_s_order))
     
-    h_snl = h_snl.merge(orderdf,how='left',on=['STND_TRRTRY_NM','SUB_CLSS_NM']) #### S_ORDER join 
+    h_snl = h_snl.merge(orderdf,how='left',on=['trrtry','SUB_CLSS']) #### S_ORDER join 
     h_snl.shape
 
     print('==-Option Level Seasonal Simulation Starts for (p,d,q) ===')
     order_temp    = []
-    terr = [v for v in h_snl['STND_TRRTRY_NM'].unique()]
+    terr = [v for v in h_snl['trrtry'].unique()]
     for ter in terr:
-        terrData   = h_snl[h_snl.STND_TRRTRY_NM==ter]
+        terrData   = h_snl[h_snl.trrtry==ter]
         optionList = list(set(terrData.KEY))
         for i in optionList:
             print('In progress' +'\n'+ter + '-----'+str(i) )
-            histData = h_snl[(h_snl.STND_TRRTRY_NM==ter)& (h_snl.KEY==i)]
+            histData = h_snl[(h_snl.trrtry==ter)& (h_snl.KEY==i)]
             if len(histData) >= 104:
-                sales = histData['RTL_QTY']
+                sales = histData['QTY']
                 sales = np.where(sales==0,1,sales)
                 sales = np.log(sales)
                 order= NonSeasonalSimulation(sales)
                 order_temp.append(ter +"_"+str(i)+"_"+str(order)) 
                 print('===SEASONAL SIMULATIONS TAGGING COMPLETE ===')
     orderdf=pd.DataFrame()
-    orderdf['STND_TRRTRY_NM']=[f.split('_')[0] for f in order_temp]
+    orderdf['trrtry']=[f.split('_')[0] for f in order_temp]
     orderdf['KEY']=[f.split('_')[1] for f in order_temp]
     orderdf['ORDER']=[f.split('_')[2] for f in order_temp]
     print(orderdf.dtypes)
     print(orderdf.head(2))
     orderdf['p'],orderdf['d'],orderdf['q']=zip(*orderdf['ORDER'].map(split_order))
-    h_snl = h_snl.merge(orderdf,how='left',on=['STND_TRRTRY_NM','KEY'])
+    h_snl = h_snl.merge(orderdf,how='left',on=['trrtry','KEY'])
     print(h_snl.shape) ### last join 
     del h_snl['d'] 
     del h_snl['D'] 
@@ -370,22 +332,15 @@ if __name__== '__main__':
                 
     h_snl = h_snl.merge(orderdf,how='left',on=['STND_TRRTRY_NM','KEY'])  ### last join 
     
-    #---------------------------------------------------------------------
-            
-                
-                
-                
-    
-    
-    
+  
     
     print('== Parameters found run S-ARIMA execution ==')
     resultF    = pd.DataFrame()
-    terrData   = h_snl[h_snl.STND_TRRTRY_NM==ter]
-    terr = [v for v in h_snl['STND_TRRTRY_NM'].unique()]
+    terrData   = h_snl[h_snl.trrtry==ter]
+    terr = [v for v in h_snl['trrtry'].unique()]
     for ter in terr:
-        histDataset1   = h_snl[h_snl.STND_TRRTRY_NM==ter]
-        leadDataset    = l[l.STND_TRRTRY_NM==ter]
+        histDataset1   = h_snl[h_snl.trrtry==ter]
+        leadDataset    = l[l.trrtry==ter]
         optionList = list(set(terrData.KEY))
         print(len(optionList))
         #optionList= optionList[0]
@@ -394,7 +349,7 @@ if __name__== '__main__':
             print('In progress' +'\n'+ter + '-----'+option )
             histData = histDataset1[histDataset1.KEY==option]
             leadData = leadDataset[leadDataset.KEY==option]            
-            sales = histData['RTL_QTY']
+            sales = histData['QTY']
             #d = int(ndiffs(sales, test='adf')) 
             #D = int(nsdiffs(sales,m=52,max_D=1))
             sales = np.where(sales==0,1,sales)
@@ -429,9 +384,8 @@ if __name__== '__main__':
                 leadData['S_ARIMA_FCST'] =0 
             result = leadData
             resultF = pd.concat([resultF,result])
-            cols = ['STND_TRRTRY_NM','KEY','TRDNG_WK_END_DT','RTL_QTY','S_ARIMA_FCST']        
+            cols = ['trrtry','KEY','date','QTY','S_ARIMA_FCST']        
             resultF = resultF[cols]
-            #resultF.to_csv(USR+'_'+CNCPT+'_'+DEPT+'_'+'SARIMA_PYOUT2020.txt')
             resultF.to_csv('sarima_sm.csv')
             print('==DONE==')
     print('==Complete==')
